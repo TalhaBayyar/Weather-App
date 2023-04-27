749ccab604c12e512dfeb58705fc438e8e38cbb7
Talha <bayyartalga@mail.com>
2023-04-27 14:42:01 +0300

    4. aşama

--- src/Context/WeatherProvider.jsx
+++ src/Context/WeatherProvider.jsx
@@ -8,15 +8,11 @@ function WeatherProvider({ children }) {
   const [cities, setCities] = useState(data)
   const [sehir, setSehir] = useState("Eskişehir")
   const [weatherinfo, setWeatherinfo] = useState([])
+ 
 
   const API_KEY = '8d23efb8d2549ba8c72cc0243a57f331';
-
   const API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${sehir}&cnt=25&units=metric&lang=tr&appid=${API_KEY}`
   
-  
-  // http://api.openweathermap.org/data/2.5/forecast?q=${sehir}&cnt=5&units=metric&appid=${API_KEY}
-  // const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${sehir}&units=metric&lang=tr&appid=${API_KEY}`
-
    useEffect(() => {
      axios.get(API_URL)
      .then((res) => {
@@ -25,17 +21,6 @@ function WeatherProvider({ children }) {
      })
      .catch((error) => console.error(error))
    }, [sehir])
-
-
- 
- 
-  
-
-
-  // const [weatherData, setWeatherData] = useState(null);
-  //  if (!weatherData) {
-  //   return <div>Loading...</div>;
-  // }
 
   return (
     <div>
